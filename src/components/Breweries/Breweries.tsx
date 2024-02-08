cf584594b3a1d0ada2dcee60408a188e1844f846
unknown <thpthv.nguyenphuduykhang@gmail.com>
2024-02-08 17:49:58 +0200

    adding some styles, displaying detail of brewery, routing

--- src/components/Breweries/Breweries.tsx
+++ src/components/Breweries/Breweries.tsx
@@ -1,5 +1,8 @@
+import { Link } from 'react-router-dom'
+
 import { Brewery } from '../../misc/type'
 import useFetch from '../../hooks/useFetch'
+import './Breweries.css'
 
 const Breweries = () => {
   const url = 'https://api.openbrewerydb.org/v1/breweries'
@@ -11,18 +14,40 @@ const Breweries = () => {
   }
 
   return (
-    <>
+    <div className="breweries__container">
       {data.map((data) => {
         console.log(data)
         return (
-          <div key={data.id}>
-            <div>{data.name}</div>
-            <div>{data.city}</div>
-            <div>-----------------------</div>
+          <div key={data.id} className="breweries__container__brewery">
+            <div>
+              <span className="breweries__container__brewery-property">Name: </span>
+              {data.name}
+            </div>
+            <div>
+              <span className="breweries__container__brewery-property">Type: </span>
+              {data.brewery_type}
+            </div>
+            <div>
+              <span className="breweries__container__brewery-property">Address: </span>
+              {data.address_1}
+            </div>
+            <div>
+              <span className="breweries__container__brewery-property">Phone: </span>
+              {data.phone}
+            </div>
+            <div>
+              <span className="breweries__container__brewery-property">Website: </span>
+              {data.website_url}
+            </div>
+            <Link to={`/breweries/${data.id}`}>
+              <button>
+                <span>Detail</span>
+              </button>
+            </Link>
           </div>
         )
       })}
-    </>
+    </div>
   )
 }
 
--- src/misc/type.ts
+++ src/misc/type.ts
@@ -1,5 +1,17 @@
 export interface Brewery {
   id: string
   name: string
+  brewery_type: string
+  address_1: string
+  phone: number
+  website_url: string
+}
+
+export interface BreweryDetailType extends Brewery {
   city: string
+  state_province: string
+  postal_code: number
+  country: string
+  state: string
+  street: string
 }
