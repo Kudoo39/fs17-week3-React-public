045771e403327e843f167b24a6ccfe66956305ab
unknown <thpthv.nguyenphuduykhang@gmail.com>
2024-02-08 22:31:42 +0200

    enhancing UI using Material UI library

--- src/components/Breweries/Breweries.tsx
+++ src/components/Breweries/Breweries.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import { Brewery } from '../../misc/type'
+import CloseIcon from '@mui/icons-material/Close'
+import SearchIcon from '@mui/icons-material/Search'
+import InputAdornment from '@mui/material/InputAdornment'
+import TextField from '@mui/material/TextField'
 import useFetch from '../../hooks/useFetch'
+import { Brewery } from '../../misc/type'
 import './Breweries.css'
 
 const Breweries = () => {
   const url = 'https://api.openbrewerydb.org/v1/breweries'
 
+  const [searchValue, setSearchValue] = useState('')
   const { data, loading } = useFetch<Brewery>(url)
 
   if (loading) {
@@ -14,40 +20,89 @@ const Breweries = () => {
   }
 
   return (
-    <div className="breweries__container">
-      {data.map((data) => {
-        console.log(data)
-        return (
-          <div key={data.id} className="breweries__container__brewery">
-            <div>
-              <span className="breweries__container__brewery-property">Name: </span>
-              {data.name}
+    <>
+      <TextField
+        label="Search..."
+        type="text"
+        size="small"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon sx={{ color: 'white' }} />
+            </InputAdornment>
+          ),
+          endAdornment: (
+            <InputAdornment position="end">
+              <CloseIcon
+                onClick={() => setSearchValue('')}
+                fontSize="small"
+                sx={{ color: 'white', cursor: 'pointer', display: searchValue.length <= 0 ? 'none' : 'block' }}
+              />
+            </InputAdornment>
+          )
+        }}
+        sx={{
+          'position': 'absolute',
+          'right': '10px',
+          'top': '10px',
+          '& label': { color: 'white' },
+          '& input': { color: 'white' },
+          '& label.Mui-focused': { color: 'WHITE' },
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderColor: 'white'
+            },
+            '&:hover fieldset': {
+              borderColor: '#bdc3c7'
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: '#bdc3c7'
+            }
+          }
+        }}
+      />
+      <div className="breweries__container">
+        {data.map((data) => {
+          console.log(data)
+          return (
+            <div key={data.id} className="breweries__container__brewery">
+              <div>
+                <span className="breweries__container__brewery-property">Name: </span>
+                {data.name}
+              </div>
+              <div>
+                <span className="breweries__container__brewery-property">Type: </span>
+                {data.brewery_type}
+              </div>
+              <div>
+                <span className="breweries__container__brewery-property">Address: </span>
+                {data.address_1}
+              </div>
+              <div>
+                <span className="breweries__container__brewery-property">Phone: </span>
+                {data.phone}
+              </div>
+              <div>
+                <span
+                  className="
+              "
+                >
+                  Website:
+                </span>
+                {data.website_url}
+              </div>
+              <Link to={`/breweries/${data.id}`}>
+                <button>
+                  <span>Detail</span>
+                </button>
+              </Link>
             </div>
-            <div>
-              <span className="breweries__container__brewery-property">Type: </span>
-              {data.brewery_type}
-            </div>
-            <div>
-              <span className="breweries__container__brewery-property">Address: </span>
-              {data.address_1}
-            </div>
-            <div>
-              <span className="breweries__container__brewery-property">Phone: </span>
-              {data.phone}
-            </div>
-            <div>
-              <span className="breweries__container__brewery-property">Website: </span>
-              {data.website_url}
-            </div>
-            <Link to={`/breweries/${data.id}`}>
-              <button>
-                <span>Detail</span>
-              </button>
-            </Link>
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
 import './Home.css'
 
 const Home = () => {
-  return <div className="home__container">This is home</div>
+  return (
+    <div className="home__container">
+      <div className="home__container__title">Homepage</div>
+      <div className="home__container__text">List of breweries</div>
+    </div>
+  )
 }
 
 export default Home
